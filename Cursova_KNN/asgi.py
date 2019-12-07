51aee8e14a8b7709c6bd39ab4e125b55ce818d0f
Mykola Matviiv <infiniti943@gmail.com>
2019-12-07 16:32:02 +0200

    Create project

---
+++ Cursova_KNN/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Cursova_KNN project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Cursova_KNN.settings')
+
+application = get_asgi_application()
---
+++ Cursova_KNN/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Cursova_KNN project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Cursova_KNN.settings')
+
+application = get_wsgi_application()
