a0738f193accc840a2c1327774adf38d4154e7ca
Mykola Matviiv <infiniti943@gmail.com>
2019-12-10 02:04:19 +0200

    Added class input

--- form/forms.py
+++ form/forms.py
@@ -5,3 +5,4 @@ class CoordinateInputForm(forms.Form):
     x = forms.IntegerField(label='x')
     y = forms.IntegerField(label='y')
     z = forms.IntegerField(label='z')
+    clas = forms.IntegerField(label='Class')
