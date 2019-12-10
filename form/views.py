67368ca23207395af0ab7d68ae60c19851b4f3cd
Mykola Matviiv <infiniti943@gmail.com>
2019-12-10 02:06:33 +0200

    Added knn process

--- form/views.py
+++ form/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from .forms import CoordinateInputForm
+from .knn import Knn
 
 class FormView(View):
 
@@ -11,9 +12,18 @@ class FormView(View):
     def post(self, request):
         form = CoordinateInputForm(request.POST)
 
+        knn = Knn(3)
+
         if form.is_valid():
             context = form.cleaned_data
+            x = context['x']
+            y = context['y']
+            z = context['z']
+            clas = context['clas']
+            knn.add_known_point(x, y, z, clas)
 
-            return render(request, 'form.html', context)
+            knn.plot()
+
+            return render(request, 'form.html', {'context': context, 'form': form})
         else:
             return render(request, 'error.html', form.errors)
