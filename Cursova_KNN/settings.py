0be02a980b39dad367a45659fa62b95bcb16ff2c
Nikola <matviivcareer@gmail.com>
2020-12-07 19:31:09 +0200

    added static

--- Cursova_KNN/settings.py
+++ Cursova_KNN/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'svqnt0&9)zmux-n-3v&pad%d2=6-4mf)(73*9phd8ttr=l(&m!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
