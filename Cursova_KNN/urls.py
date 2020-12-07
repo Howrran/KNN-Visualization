b7a528ad558be8a2fe1174220111d498e60a5fb2
Nikola <matviivcareer@gmail.com>
2020-12-07 19:47:57 +0200

    fix

--- Cursova_KNN/urls.py
+++ Cursova_KNN/urls.py
@@ -20,6 +20,7 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('form/', include('form.urls')),
-    path('', RedirectView.as_view(url='/form'))
+    path('', RedirectView.as_view(url='/form')),
+    path('/', RedirectView.as_view(url='/form')),
 
 ]
